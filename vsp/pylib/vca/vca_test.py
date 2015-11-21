0e89cbbcb59cc286b8b53dfecfd7843c07a82ec9
Sabyasachi Sengupta <sabyasg@yahoo.com>
2015-11-21 00:04:07 -0800

    vsp-pylib-vca_test: Add vca_test.TEST.run

--- vsp/pylib/vca/vca_test.py
+++ vsp/pylib/vca/vca_test.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python
+
+
+import sys
+
+sys.path.append("/usr/local/openvswitch/pylib/system")
+import shell
+
+class TEST(object):
+	def __init__(self, test_id, test_desc, handler, param):
+		self.test_id = test_id
+		self.test_desc = test_desc
+		self.handler = handler
+		self.param = param
+
+	def run(self):
+		print "### Running test #" + str(self.test_id) + ": " + self.test_desc
+		self.handler(self.param)
+		print 
