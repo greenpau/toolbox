e19fd3b05e95ee1d268f969abac1de1468761075
Sabyasachi Sengupta <sabyasg@yahoo.com>
2015-11-23 15:28:17 -0800

    vsp-pylib-vca_test: Normalize banner printing in TEST.run()

--- vsp/pylib/vca/vca_test.py
+++ vsp/pylib/vca/vca_test.py
@@ -8,12 +8,16 @@ import shell
 
 class TEST(object):
 	def __init__(self, test_id, test_desc, handler, param):
-		self.test_id = test_id
+		self.test_id = str(test_id)
 		self.test_desc = test_desc
 		self.handler = handler
 		self.param = param
 
 	def run(self):
-		print "### Running test #" + str(self.test_id) + ": " + self.test_desc
-		self.handler(self.param)
-		print 
+		print "Testcase ID #" + str(self.test_id) + ": " + self.test_desc
+		self.passed = self.handler(self.param)
+		if (self.passed) :
+			print "######################### Testcase ID #" + self.test_id + ": PASSED ############################"
+		else:
+			print "######################### Testcase ID #" + self.test_id + ": FAILED ############################"
+		print
