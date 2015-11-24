dd1feea76a24fc08ddee03be3647a055bf9b3cae
Sabyasachi Sengupta <sabyasg@yahoo.com>
2015-11-23 23:41:17 -0800

    vsp-pylib-vca_test: Add vca_test.SUITE

--- vsp/pylib/vca/vca_test.py
+++ vsp/pylib/vca/vca_test.py
@@ -6,6 +6,24 @@ import sys
 sys.path.append("/usr/local/openvswitch/pylib/system")
 import shell
 
+class SUITE(object):
+	def __init__(self, suite_name):
+		self.suite_name = suite_name
+		self.tests = []
+
+	def register_test(self, TEST):
+		self.tests.append(TEST)
+
+	def print_summary(self):
+		n_passed = 0
+		n_failed = 0
+		for test in self.tests:
+			if (test.passed == True):
+				n_passed = n_passed + 1
+			else:
+				n_failed = n_failed + 1
+		print "Num tests: " + str(n_passed + n_failed) + " (Passed: " + str(n_passed) + ", Failed: " + str(n_failed) + ")"
+
 class TEST(object):
 	def __init__(self, test_id, test_desc, handler, param):
 		self.test_id = str(test_id)
