58f173d7d14876b3c7d1adc5c8faae23e01e0433
Sabyasachi Sengupta <sabyasg@yahoo.com>
2015-12-09 13:09:27 -0800

    vsp-pylib-system-net: Add packet_create and helper functions that creates a dummy TCP/IP packet.

--- vsp/pylib/system/net.py
+++ vsp/pylib/system/net.py
@@ -6,6 +6,8 @@ sys.path.append("/usr/local/openvswitch/pylib/system")
 import os
 import subprocess
 import shell
+import socket
+import struct
 
 def __get_ifconfig_iface_ip(iface):
 	ifconfig_out = shell.execute(["ifconfig", iface]).splitlines()
@@ -152,3 +154,98 @@ def ipaddr2hex(ip):
 	b = hex(int(a[0])) + hex(int(a[1])) + hex(int(a[2])) + hex(int(a[3]))
 	b = b.replace('0x', '')
 	return b
+
+def checksum (msg) :
+	if (len(msg)%2 != 0) :
+		msg2 = msg + '\x00' 
+	else :
+		msg2 = msg
+	s = 0
+	for i in range (0, len(msg2), 2):
+		w = (ord(msg2[i])<<8) + (ord(msg2[i+1]))
+		s = s + w
+	s = (s >> 16) + (s & 0xffff)
+	s = (s >> 16) + (s & 0xffff)
+	return ~s & 0xffff
+
+def unpack_helper (fmt, data) :
+	size = struct.calcsize(fmt)
+	return struct.unpack(fmt, data[:size])+tuple([data[size:]])
+
+def tcp_encap (ip_src, ip_dst, tcp_src_port, tcp_dst_port, tcp_payload) :
+	ip_src_addr = socket.inet_aton (ip_src) 
+	ip_dst_addr = socket.inet_aton (ip_dst)
+	ip_proto = socket.IPPROTO_TCP
+	# tcp_src_port 
+	# tcp_dst_port
+	tcp_seq = 0
+	tcp_ack_seq = 0
+	tcp_doff = 5 # 4 bit field, size of tcp header, 5 * 4 = 20 bytes
+	# tcp flags
+	tcp_fin = 1
+	tcp_syn = 0
+	tcp_rst = 0
+	tcp_psh = 0
+	tcp_ack = 0
+	tcp_urg = 0
+	tcp_window = 0
+	tcp_csum = 0
+	tcp_urg_ptr = 0
+	tcp_offset_res = (tcp_doff << 4) + 0
+	tcp_flags = tcp_fin + (tcp_syn << 1) + (tcp_rst << 2) + (tcp_psh << 3) + (tcp_ack << 4) + (tcp_urg << 5)
+	tcp_hdr = struct.pack ('!HHLLBBHHH', tcp_src_port, tcp_dst_port, tcp_seq, tcp_ack_seq, tcp_offset_res, tcp_flags, tcp_window, tcp_csum, tcp_urg_ptr)
+	tcp_len = len (tcp_hdr) + len (tcp_payload)
+	pseudo_hdr = struct.pack ('!4s4sBBH', ip_src_addr, ip_dst_addr, tcp_csum, ip_proto, tcp_len) + tcp_hdr + tcp_payload
+	tcp_csum = checksum (pseudo_hdr)
+	tcp_hdr = struct.pack ('!HHLLBBHHH', tcp_src_port, tcp_dst_port, tcp_seq, tcp_ack_seq, tcp_offset_res, tcp_flags, tcp_window, tcp_csum, tcp_urg_ptr)
+	return tcp_hdr+tcp_payload
+
+def tcp_decap(tcp_data) :
+	# (tcp_src_port, tcp_dst_port, tcp_seq, tcp_ack_seq, tcp_offset_res, tcp_flags, tcp_window, tcp_csum, tcp_urgptr, tcp_payload)
+	return unpack_helper ('!HHLLBBHHH', tcp_data)
+
+def ip_encap (ip_src, ip_dst, ip_proto, ip_payload) :
+	# ip header 
+	ip_ihl = 5
+	ip_ver = 4
+	ip_tos = 0
+	ip_tot_len = 20 + len(ip_payload)
+	ip_id = 12345
+	ip_frag_off = 0
+	ip_ttl = 255
+	# ip_proto 
+	ip_csum = 0
+	ip_src_addr = socket.inet_aton(ip_src)
+	ip_dst_addr = socket.inet_aton(ip_dst)
+	ip_ihl_ver = (ip_ver << 4) + ip_ihl
+	ip_hdr = struct.pack ('!BBHHHBBH4s4s', ip_ihl_ver, ip_tos, ip_tot_len, ip_id, ip_frag_off, ip_ttl, ip_proto, ip_csum, ip_src_addr, ip_dst_addr)
+	ip_csum = checksum (ip_hdr)
+	ip_hdr = struct.pack ('!BBHHHBBH4s4s', ip_ihl_ver, ip_tos, ip_tot_len, ip_id, ip_frag_off, ip_ttl, ip_proto, ip_csum, ip_src_addr, ip_dst_addr)
+	return ip_hdr+ip_payload
+
+def ip_decap (ip_data) :
+	# (ip_ihl_ver, ip_tos, ip_tot_len, ip_id, ip_frag_off, ip_ttl, ip_proto, ip_csum, ip_src_addr, ip_dst_addr, ip_payload)
+	return unpack_helper ('!BBHHHBBH4s4s', ip_data)
+
+def mac2bin(mac) : 
+	return mac.replace(':','').decode('hex')
+
+def eth_encap(eth_src, eth_dst, eth_type, eth_payload) :
+	# eth header
+	eth_type    = 0x0800
+	eth_mac_src = mac2bin (eth_src)
+	eth_mac_dst = mac2bin (eth_dst)
+	eth_hdr = struct.pack ('!6s6sH', eth_mac_dst, eth_mac_src, eth_type)
+	return eth_hdr+eth_payload
+
+def eth_decap (eth_data) :
+	# (eth_mac_dst, eth_mac_src, eth_type, eth_payload)
+	return unpack_helper ('!6s6sH', eth_data)
+
+def packet_create (cookie, eth_src, ip_src, eth_dst, ip_dst, ip_payload) :
+	packet = eth_encap(eth_src, eth_dst, 0x0800,
+			   ip_encap(ip_src, ip_dst, socket.IPPROTO_TCP,
+				    tcp_encap(ip_src, ip_dst,
+					      cookie, 0, ip_payload)))
+    
+	return packet
