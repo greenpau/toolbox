88945f65d4e26b9d88f8e192bf1122649e954ab6
Sabyasachi Sengupta <sabyasg@yahoo.com>
2015-12-29 15:54:52 -0800

    vsp-pylib: Add get_ovs_version to ovs_helper that returns the OVS SDK version.

--- vsp/pylib/ovs/ovs_helper.py
+++ vsp/pylib/ovs/ovs_helper.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 import os
+import shell
 
 def set_tnl_key(tnl_type):
 	if (tnl_type == "vxlan"):
@@ -28,6 +29,22 @@ def set_defaults(home, progname):
 	br = "alubr0"
 	vlan_id = str(1)
 	return ovs_path, hostname, os_release, logfile, br, vlan_id
+
+def get_ovs_version(ovs_path):
+	cmd = [ ovs_path + "/ovs-appctl", "--vers" ]
+	ovs_vers_out = shell.execute(cmd).splitlines()
+	match_pattern = "Open vSwitch base release:"
+	field = 4
+	vers = 0x230
+	for line in ovs_vers_out:
+		line_tok = line.split()
+		if (line_tok == None) or (line_tok == []):
+			continue
+		if (line.find(match_pattern) < 0):
+			continue
+		vers = line_tok[field]
+		break
+	return vers
 
 def print_defaults(ovs_path, os_release, hostname, logfile):
 	print "Hostname: " + hostname
