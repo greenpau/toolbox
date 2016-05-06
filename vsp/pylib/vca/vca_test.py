c6165d7142733cf54cff098d1da4bb0556efd6cc
Sabyasachi Sengupta <sabyasg@yahoo.com>
2016-05-06 14:31:58 -0700

    vsp-pylib-vca-test: Add number of sub-tests at the completion of each testcase with TestcaseID banner.

--- vsp/pylib/vca/vca_test.py
+++ vsp/pylib/vca/vca_test.py
@@ -65,7 +65,7 @@ class TEST(object):
 		print "Testcase ID #" + str(self.test_id) + ": " + self.test_desc
 		self.passed, self.n_sub_tests = self.handler(self.param)
 		if (self.passed) :
-			print "######################### Testcase ID #" + self.test_id + ": PASSED ############################"
+			print "################## Testcase ID #" + self.test_id + ": PASSED (sub-tests: " + str(self.n_sub_tests) + ") ####################"
 		else:
-			print "######################### Testcase ID #" + self.test_id + ": FAILED ############################"
+			print "################## Testcase ID #" + self.test_id + ": FAILED (sub-tests: " + str(self.n_sub_tests) + ") ####################"
 		print
