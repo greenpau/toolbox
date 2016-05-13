e3627e7fec04e2dc05526388cb270e97d2c5618f
Sabyasachi Sengupta <sabyasg@yahoo.com>
2016-05-13 10:38:56 -0700

    vsp-pylib-ovs_flows: Add Flows.get_n_pkts_hit_by_table and Flows.get_n_pkts_by_table APIs that returns number of packets by specified table or all n_packets if table_id=None

--- vsp/pylib/ovs/ovs_flows.py
+++ vsp/pylib/ovs/ovs_flows.py
@@ -10,6 +10,7 @@ class Flows(object):
 	def __init__(self, ovs_path, logfd):
 		self.logfd = logfd
 		self.ofctl_path = ovs_path + "/ovs-ofctl"
+		self.appctl_path = ovs_path + "/ovs-appctl"
 		self.ofproto = ovs_ofproto.OFProto(ovs_path)
 
 	def set_flow_execute(self, hdr, br, flow_str):
@@ -65,3 +66,48 @@ class Flows(object):
 		port_ofp_port = self.ofproto.ofp_port(br, port)
 		flow_str = "in_port=" + port_ofp_port + ",idle_timeout=0,action=mod_dl_src:" + dst_mac + ",mod_dl_dst:" + src_mac + ",mod_nw_src:" + dst_ip + ",mod_nw_dst:" + src_ip + ",output:" + port_ofp_port
 		self.set_flow_execute(hdr, br, flow_str)
+
+	def __parse_dump_flows(self, br, table_id, col):
+		cmd = [ self.appctl_path, "bridge/dump-flows", br ]
+		out = shell.execute(cmd).splitlines()
+		n_flows = 0
+		out_flows = []
+		for l in out:
+			toks = l.split()
+			if (toks[0].find("table_id") < 0):
+				continue
+			this_table_id = toks[0].split("=")[1].replace(",", "")
+			if (this_table_id == str(254) or
+			    this_table_id == str(0)):
+				continue
+			if (table_id != None):
+				if (this_table_id != str(table_id)):
+					continue
+			n_flows = n_flows + 1
+			if (col != None):
+				out_flows.append([this_table_id, toks[int(col)]])
+			else:
+				out_flows.append([this_table_id, l])
+		return n_flows, out_flows
+
+	def get_n_pkts_by_table(self, br, table_id):
+		n_flows, flows = self.__parse_dump_flows(br, table_id, "2")
+		out_flows = []
+		n_flows_pkt = 0
+		for f in flows:
+			n_pkts = int(f[1].split("=")[1].replace(",", ""))
+			out_flows.append([int(f[0]), n_pkts])
+			n_flows_pkt = n_flows_pkt + 1
+		return n_flows_pkt, out_flows
+
+	def get_n_pkts_hit_by_table(self, br, table_id):
+		n_flows, flows = self.__parse_dump_flows(br, table_id, "2")
+		out_flows = []
+		n_flows_pkt_hits = 0
+		for f in flows:
+			n_pkts = int(f[1].split("=")[1].replace(",", ""))
+			if (n_pkts == 0):
+				continue
+			out_flows.append([int(f[0]), n_pkts])
+			n_flows_pkt_hits = n_flows_pkt_hits + 1
+		return n_flows_pkt_hits, out_flows
