158334372344dd12a4da331975acb79a8ca3e97e
Sabyasachi Sengupta <ssengupta@arubanetworks.com>
2018-02-13 16:16:27 -0800

    p4-tools-{libp4,preview}: Add "-c all" option to preview (display content of all changelists)

--- p4/tools/libp4.sh
+++ p4/tools/libp4.sh
@@ -6,3 +6,25 @@ get_branch() {
 	echo "${branch}"
 }
 
+get_clid_list_pending() {
+	local user=$1
+	local client=$2
+	clid_list="`p4 changes -u ${user} | grep ${client} | grep pending | awk '{print $2}' | sort -u`"
+	echo $clid_list
+}
+
+get_clid_n_pending() {
+	local user=$1
+	local client=$2
+	clid_list_pending="`get_clid_list_pending ${user} ${client}`"
+	n_clid_pending=`echo ${clid_list_pending} | awk 'END{print NF}'`
+	echo ${n_clid_pending}
+}
+
+get_clid_most_recent() {
+	local user=$1
+	local client=$2
+	clid_list_pending="`get_clid_list_pending ${user} ${client}`"
+	clid=`echo ${clid_list_pending} | awk '{print $NF}'`
+	echo ${clid}
+}
